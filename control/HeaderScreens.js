cf72ed2ce461506bdbbdaa1d7d7818432d14e7db
Lucas Guimarães <guga.lucas2015@gmail.com>
2023-03-27 17:08:19 -0300

    Icone de Avatar
    Consegui colocar um Ícone de avatar no Header e deixá-lo clicavel.

--- control/HeaderScreens.js
+++ control/HeaderScreens.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Appbar } from 'react-native-paper';
-import NotaScreen from '../view/notaScreen';
+import { Appbar, Avatar, TouchableRipple } from 'react-native-paper';
+import { Alert, Platform } from 'react-native';
 
 const TestHeader = () => {
   return (
         <Appbar.Header mode='center-aligned'>
+        <TouchableRipple onPress={()=>{Alert.alert("Teste ícone","Testando o Touch do ícone.")}}>
+          <Avatar.Text size={36} label="TT"/>
+        </TouchableRipple>
         <Appbar.Content title="Title" />
         </Appbar.Header>
   );
