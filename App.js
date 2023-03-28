cafa201ec918ef3a3bbbf6b38885d630f6493f01
Lucas Guimarães <guga.lucas2015@gmail.com>
2023-03-28 17:17:35 -0300

    Tentativa de Navegar
    Tentei usar o React-Navigation, mas não fomos bem sucedido em partes.
    
    Conseguimos aplicar ele em App.js.
    
    Entretanto, não foi possível realizar a transição entre as telas userScreen e notaScreen

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import UserScreen from './view/userScreen';
 import NotaScreen from './view/notaScreen';
 import TestHeader from './control/HeaderScreens';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    //<UserScreen />
-    <SafeAreaProvider>
-      <TestHeader/>
-      <NotaScreen />
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="notaScreen" component={NotaScreen} options={{header: TestHeader}}/>
+        <Stack.Screen name="userScreen" component={UserScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- control/HeaderScreens.js
+++ control/HeaderScreens.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Appbar, Avatar, TouchableRipple } from 'react-native-paper';
-import { Alert, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const TestHeader = () => {
+  const navigation = useNavigation();
   return (
         <Appbar.Header mode='center-aligned'>
-        <TouchableRipple onPress={()=>{Alert.alert("Teste ícone","Testando o Touch do ícone.")}}>
+        <TouchableRipple onPress={()=> navigation.navigate('UserScreen', {name: 'UserScreen'})}>
           <Avatar.Text size={36} label="TT"/>
         </TouchableRipple>
         <Appbar.Content title="Title" />
