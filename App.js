ef832079d3e2c0c83e498bc31fd3312c8e6046cb
Lucas Guimarães <lucas.gu1mara3s@outlook.com>
2023-04-11 11:37:30 -0300

    Arrumando o Navigator
    Agora é para funcionar o Stack com o Tab juntos.
    
    Ainda não foi possível realizar o teste, mas deve ser o suficiente isso.

--- App.js
+++ App.js
@@ -6,10 +6,39 @@ import BarraNavigation from './control/barraNavigation'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Avatar } from 'react-native-paper';
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
 
+/**
+ * 
+ * @returns Retorna um pilha de Navegação para a tela NotaScreen
+ */
+function NotaStackScreen(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="notaScreen" component={NotaScreen} options={{header: TestHeader}}/>
+      <Stack.Screen name="userScreen" component={UserScreen} options={{headerTitleAlign: 'center', headerTitle: 'Usuário'}}/>
+    </Stack.Navigator>
+  )
+}
+
+/**
+ * 
+ * @returns Retorna um pilha de Navegação para a tela FaltaScreen
+ */
+function FaltaStackScreen(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="faltaScreen" component={FaltaScreen} options={{headerTitleAlign: 'center', headerTitle: 'Faltas', header: TestHeader}}/>
+      <Stack.Screen name="userScreen" component={UserScreen} options={{headerTitleAlign: 'center', headerTitle: 'Usuário'}}/>
+    </Stack.Navigator>
+  )
+}
+
+/**
+ * 
+ * @returns Retorna a aplicação utilizando um NavigationContainer e Tab.Navigator
+ */
 export default function App() {
   return (
     <NavigationContainer>
@@ -19,9 +48,8 @@ export default function App() {
         <Stack.Screen name="faltaScreen" component={FaltaScreen} options={{headerTitleAlign: 'center', headerTitle: 'Faltas'}}/>
   </Stack.Navigator>*/}
    <Tab.Navigator> 
-  <Tab.Screen name='Notas' component={NotaScreen} options={{header: TestHeader}}/>
-  <Tab.Screen name='Faltas' component={FaltaScreen} options={{headerTitleAlign: 'center', headerTitle: 'Faltas'}}/>
-  <Tab.Screen name='userScreen' component={UserScreen} options={{headerTitleAlign: 'center', headerTitle: 'Usuário'}}/>
+  <Tab.Screen name='Notas' component={NotaStackScreen}/>
+  <Tab.Screen name='Faltas' component={FaltaStackScreen}/>
     </Tab.Navigator>
     </NavigationContainer>
   );
